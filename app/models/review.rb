e6ad045d48f1bc33d360840ba171ac6e49e8c6e8
vagrant <vagrant@vagrant.vm>
2019-09-23 17:44:24 +0000

    book review app

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to :book
+  belongs_to :user
+
+   validates :body, presence: true
+   validates_uniqueness_of :book_id, scope: :user_id
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :books do
+    resources :reviews
+  end
+
+  devise_for :users
+  root 'books#index'
+
+end
